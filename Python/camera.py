9bf9693ec48786d548a8f0bccf4f4884fc8c6fff
Proxd110 <castilloesquiveljoseluis@gmail.com>
2024-07-03 03:04:29 -0600

    Add a way to win which is the white exit

--- Python/camera.py
+++ Python/camera.py
@@ -28,10 +28,10 @@ class Camera:
         if direction == 'right':
             self.pos[0] += amount * cos(radians(self.rot[0]))
             self.pos[2] -= amount * -sin(radians(self.rot[0]))
-        if direction == 'up':
-            self.pos[1] += amount
-        if direction == 'down':
-            self.pos[1] -= amount
+        #if direction == 'up':
+        #    self.pos[1] += amount
+        #if direction == 'down':
+        #    self.pos[1] -= amount
 
     def rotate(self, dx, dy):
         self.rot[0] += dx
